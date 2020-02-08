72083ef71547b1fe59c580ee4dbf19ea04e21c35
JHPAT <sanhool@live.com.mx>
2020-02-08 03:37:07 -0500

    visor_imagenes

--- app/src/main/java/com/example/didatiacapp/MainActivity.java
+++ app/src/main/java/com/example/didatiacapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.didatiacapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView i1,i2;
+Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i1=findViewById(R.id.img_1);
+        i2=findViewById(R.id.img_2);
+
+
     }
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.img_1:
+                intent= new Intent(this, menu_juegos.class);
+                this.startActivity(intent);
+                break;
+
+            case R.id.img_2:
+                intent= new Intent(this, visor_imagenes.class);
+                this.startActivity(intent);
+                break;
+        }
+    }
+
 }
