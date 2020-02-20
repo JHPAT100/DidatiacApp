4feca2eedcb25f2522972a30c35af59183bec71a
JHPAT <sanhool@live.com.mx>
2020-02-19 20:28:46 -0500

    cambios_xd

---
+++ app/src/main/java/com/example/didatiacapp/info_app.java
@@ -0,0 +1,14 @@
+package com.example.didatiacapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class info_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_app);
+    }
+}
